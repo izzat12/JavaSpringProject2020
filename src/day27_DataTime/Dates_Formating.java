ea729ae503b291759ed6b5c863427d311d333a8f
izzat12 <Izzatpatidinov@gmail.com>
2020-04-23 12:42:01 -0400

    day27_DataTime

--- src/day27_DataTime/Dates_Formating.java
+++ src/day27_DataTime/Dates_Formating.java
@@ -11,6 +11,19 @@ public class Dates_Formating {
         LocalDate date1 = LocalDate.now();
         System.out.println(date1.format(dtf));
 
+        System.out.println("=====================================");
+
+        /*
+        create a date called birthDay: month/days/year
+         */
+
+
+        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("MMM/dd/yy");
+
+        LocalDate date2 = LocalDate.of(1994,12,30);
+        String str1 = date2.format(dtf2);
+        System.out.println(str1);
+
 
     }
 }
