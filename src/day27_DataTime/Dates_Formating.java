ebc34f7e63ca5f7c5221d7b56c1841f1f57f76f7
izzat12 <Izzatpatidinov@gmail.com>
2020-04-23 12:01:21 -0400

    day27_DataTime

--- src/day27_DataTime/Dates_Formating.java
+++ src/day27_DataTime/Dates_Formating.java
@@ -1,7 +1,16 @@
 package day27_DataTime;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Dates_Formating {
     public static void main(String[] args) {
+
+        // To format the dates
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MMM/yyyy");
+        LocalDate date1 = LocalDate.now();
+        System.out.println(date1.format(dtf));
+
 
     }
 }
