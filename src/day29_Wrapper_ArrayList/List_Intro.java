ef205bbb199a5fe2b09123654f1aa65213195835
izzat12 <Izzatpatidinov@gmail.com>
2020-04-27 14:50:29 -0400

    day29 Class

---
+++ src/day29_Wrapper_ArrayList/List_Intro.java
@@ -0,0 +1,18 @@
+package day29_Wrapper_ArrayList;
+
+import java.util.ArrayList;
+
+public class List_Intro {
+    public static void main(String[] args) {
+//       ArrayList <DataType> listName = new ArrayList<DataType>();
+
+        ArrayList<String> scores = new ArrayList<>();
+
+        int[] arr = new int[3];
+
+
+
+
+
+    }
+}
