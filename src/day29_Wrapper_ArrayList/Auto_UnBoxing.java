20aab350239b86b0f5f9b970fd17c2b8a9fc534a
izzat12 <Izzatpatidinov@gmail.com>
2020-04-27 10:42:02 -0400

    day29 Class

---
+++ src/day29_Wrapper_ArrayList/Auto_UnBoxing.java
@@ -0,0 +1,25 @@
+package day29_Wrapper_ArrayList;
+
+public class Auto_UnBoxing {
+    public static void main(String[] args) {
+
+        // Autoboxing
+     int a = 100; // primitive
+     Integer b = a;     //Autoboxing
+
+//====================================
+
+        //Unboxing: converts wrapper class value into primitive value
+        Byte by1 = 98;
+
+        short short1 = by1;
+        byte b2 = by1;
+        int int1 = by1;
+ //===================================
+
+
+
+
+
+    }
+}
