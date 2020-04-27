7fe4740962f52f157b40ff2bc4e482880bda1188
izzat12 <Izzatpatidinov@gmail.com>
2020-04-27 15:28:36 -0400

    day29 Class

--- src/day29_Wrapper_ArrayList/List_Intro.java
+++ src/day29_Wrapper_ArrayList/List_Intro.java
@@ -6,9 +6,27 @@ public class List_Intro {
     public static void main(String[] args) {
 //       ArrayList <DataType> listName = new ArrayList<DataType>();
 
-        ArrayList<String> scores = new ArrayList<>();
+        ArrayList<Integer> scores = new ArrayList<>(); //  size: 0
+        //                                             add five elements , size: 5
+        //                                             remove 2 elements size: 3
 
-        int[] arr = new int[3];
+        // add();
+        scores.add(10);  //AutoBoxing   // size: 1
+        scores.add(20);
+        scores.add(30);
+
+        System.out.println(scores);
+
+
+        Integer a1 = scores.get(2); // none
+
+        int a2 = scores.get(2);   // unboxing
+
+        double a3 = scores.get(2); // unboxing
+
+        System.out.println(a3);
+
+
 
 
 
