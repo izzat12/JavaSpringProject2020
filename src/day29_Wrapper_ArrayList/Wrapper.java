a60ef640f88ef0ba164348ffb1d7eaa05c414e64
izzat12 <Izzatpatidinov@gmail.com>
2020-04-27 10:29:55 -0400

    Day29 Class

---
+++ src/day29_Wrapper_ArrayList/Wrapper.java
@@ -0,0 +1,34 @@
+package day29_Wrapper_ArrayList;
+
+public class Wrapper {
+    public static void main(String[] args) {
+
+        int a = 100;         //primitive type
+
+        Integer b = a;     // wrapper class
+   ///////////////////////////////////////
+
+        short c = 200;
+        int in = c;
+
+      //  Integer In = c;   // Integer is dedicated to only int primitive
+    //=================================================================
+
+        int a1 = 20;
+        float f1 = a1;
+
+        Float F1 = f1;
+    //=================================================================
+
+    Integer I1 = 200;
+    int a2 = I1;
+
+    //=================================================
+
+    Byte B1 = 126;
+    int a3 = B1;
+        System.out.println(a3);
+
+
+    }
+}
--- src/pratice/hello.java
+++
@@ -1,7 +0,0 @@
-package pratice;
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("asdasdasdas");
-    }
-}
