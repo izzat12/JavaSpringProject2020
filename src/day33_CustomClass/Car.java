b8130ec9e3011cb7931254722b5d269f55b8a901
izzat12 <Izzatpatidinov@gmail.com>
2020-05-04 15:00:17 -0400

    Day33 Class

--- src/day33_CustomClass/Car.java
+++ src/day33_CustomClass/Car.java
@@ -1,6 +1,5 @@
 package day33_CustomClass;
 
-import java.time.Year;
 
 public class Car {
 
@@ -14,15 +13,34 @@ public void start(){
 }
 
 
+
 public void drive(){
     System.out.println(Brand + " "+ Model + " is driving on the road");
 }
+
+
 
 public void getCarInfo(){
     System.out.println(year + " " + Brand + " " + Model + " " + color);
 }
 
 
+
+public void setCarInfo (String carBrand, String carModel, int carYear, String carcolor){
+
+    Brand = carBrand;
+    Model = carModel;
+    year = carYear;
+    color = carcolor;
+}
+
+
+
+public String toString(){
+    String result = year + " " + Brand + " " + Model + " " + color;
+    return result;
+
+}
 
 
 
---
+++ src/day33_CustomClass/carObjects2.java
@@ -0,0 +1,48 @@
+package day33_CustomClass;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class carObjects2 {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+        car1.setCarInfo("Toyota","Highlander", 2020,"Blue");
+        car1.getCarInfo();
+
+        Car car2 = new Car();
+        car2.setCarInfo("BMW","X6",2020,"Black");
+        car2.getCarInfo();
+
+        Car car3 = new Car();
+        car3.setCarInfo("Honda","Civic",2020,"White");
+        car3.getCarInfo();
+
+        System.out.println("=============================================");
+
+        Car[] cars = {car1,car2,car3};
+
+        for(int i = 0; i <cars.length; i++){
+
+            cars[i].getCarInfo();
+        }
+
+        System.out.println("====================================");
+
+        for (Car eachCar : cars){
+
+            eachCar.getCarInfo();
+
+        }
+
+        System.out.println("=================================");
+
+        System.out.println(car1);
+
+
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3,4));
+        System.out.println(list1);
+
+    }
+}
